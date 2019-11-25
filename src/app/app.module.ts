535aa0a6f8de2b00c9b1bc9abaecb86071823a38
zeineb97 <zeinebchabchoub97@gmail.com>
2019-11-24 22:04:13 -0800

    sign up form: add-user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment.prod';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { NgxLoadingModule } from 'ngx-loading';
+import { AddUserComponent } from './users/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    // ReactiveFormsModule,
+    AgGridModule.withComponents([]),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    NgxLoadingModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCC7BzxhEVZaei3wqvaQcdtJxdBBIZEmKw",
+    authDomain: "projetangular-d9bca.firebaseapp.com",
+    databaseURL: "https://projetangular-d9bca.firebaseio.com",
+    projectId: "projetangular-d9bca",
+    storageBucket: "projetangular-d9bca.appspot.com",
+    messagingSenderId: "51585298632",
+    appId: "1:51585298632:web:60665c52c412c88f5df5fd",
+    measurementId: "G-5GZNFF2BHX"
+  }
 };
