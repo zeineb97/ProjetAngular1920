aeaf4dffe89dd6fee0cfa98b1dddc0dcd80287cd
zeineb97 <zeinebchabchoub97@gmail.com>
2019-12-02 00:14:11 -0800

    auth (sign in / up / out

---
+++ src/app/users/auth/auth.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  email: string;
+  password: string;
+
+  ngOnInit() {
+  }
+
+  constructor(public authenticationService: AuthenticationService) { }
+
+  signUp() {
+    this.authenticationService.SignUp(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signIn() {
+    this.email = '';
+    this.password = '';
+  }
+
+  signOut() {
+    this.authenticationService.SignOut();
+  }
+
+}
