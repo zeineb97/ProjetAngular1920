3120a89627820aa591be4036211307aa08ed458f
olfa-koubaa <koubaaolfa1@gmail.com>
2019-12-03 22:26:21 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DefaultLayoutComponent} from './default-layout/default-layout.component';
+import {AddComponent} from './add/add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : DefaultLayoutComponent,
+    children : [
+      {
+        path : '',
+        component : AddComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
