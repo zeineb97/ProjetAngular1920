3ecaad82328cb8e090ea6fdd075d616170429c41
zeineb97 <zeinebchabchoub97@gmail.com>
2019-12-03 23:31:47 -0800

    path : /adduser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DefaultLayoutComponent} from './default-layout/default-layout.component';
-import {AddComponent} from './add/add.component';
-
+import { DefaultLayoutComponent } from './default-layout/default-layout.component';
+import { AddComponent } from './add/add.component';
+import { AddUserComponent } from './users/add-user/add-user.component'
+import { from } from 'rxjs';
 
 const routes: Routes = [
   {
-    path : '',
-    component : DefaultLayoutComponent,
-    children : [
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
       {
-        path : '',
-        component : AddComponent
+        path: '',
+        component: AddComponent
       }
     ]
+  },
+  {
+    path: 'adduser',
+    component: AddUserComponent
   }
+
 ];
 
 @NgModule({
