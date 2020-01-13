a82a2d8d490711a326e86377595f85d5b82c3b95
meriamtarhouni <37798355+meriamtarhouni@users.noreply.github.com>
2020-01-14 00:59:44 +0100

    addOffre modifications

--- src/app/add-offre/add-offre.component.ts
+++ src/app/add-offre/add-offre.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-offre.component.css']
 })
 export class AddOffreComponent implements OnInit {
-
+   imgSrc = 'assets/Images/default.png';
+   selectedImage: any = null ;
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  showPreview(event: any) {
+      if (event.target.files && event.target.files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e: any ) => this.imgSrc = e.target.result;
+        reader.readAsDataURL(event.target.files[0]);
+        this.selectedImage = event.target.files[0];
+      } else {
+        this.imgSrc = 'assets/Images/default.png';
+        this.selectedImage = null;
+      }
+
+  }
 }
