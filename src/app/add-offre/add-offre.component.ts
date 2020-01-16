a38806195afc97ace826055b2c9de32776c6c127
zeineb <zeinebchabchoub97@gmail.com>
2020-01-16 00:23:36 -0800

    UI-UX

--- src/app/add-offre/add-offre.component.ts
+++ src/app/add-offre/add-offre.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {OffreService} from "../services/offre.service";
-import {Offre} from "../model/Offre";
-import {Router} from "@angular/router";
+import { NgForm } from '@angular/forms';
+import { OffreService } from "../services/offre.service";
+import { Offre } from "../model/Offre";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-add-offre',
@@ -10,39 +10,39 @@ import {Router} from "@angular/router";
   styleUrls: ['./add-offre.component.css']
 })
 export class AddOffreComponent implements OnInit {
-   imgSrc = 'assets/Images/default.png';
-   selectedImage: any = null ;
-   selectedImagesList: any = null ;
-  constructor(private offreService: OffreService,private router : Router) { }
+  imgSrc = 'assets/Images/default.png';
+  selectedImage: any = null;
+  selectedImagesList: any = null;
+  constructor(private offreService: OffreService, private router: Router) { }
 
   ngOnInit() {
 
   }
   addOffre(formulaire: NgForm) {
     const artisanId = localStorage.getItem('token');
-    const offre =  new Offre(artisanId, formulaire.value.nom, formulaire.value.description,'img',['img1','img2'],formulaire.value.prix);
+    const offre = new Offre(artisanId, formulaire.value.nom, formulaire.value.description, 'https://ae01.alicdn.com/kf/HTB1R.kLPVXXXXajXpXXq6xXFXXX9.jpg', ['https://ae01.alicdn.com/kf/HTB1R.kLPVXXXXajXpXXq6xXFXXX9.jpg', 'https://ae01.alicdn.com/kf/HTB1R.kLPVXXXXajXpXXq6xXFXXX9.jpg'], formulaire.value.prix);
     this.offreService.createOffre(offre).then(() => {
       this.router.navigate(['']);
     });
   }
   showPreview(event: any) {
-      if (event.target.files && event.target.files[0]) {
-        const reader = new FileReader();
-        reader.onload = (e: any ) => this.imgSrc = e.target.result;
-        reader.readAsDataURL(event.target.files[0]);
-        this.selectedImage = event.target.files[0];
-      } else {
-        this.imgSrc = 'assets/Images/default.png';
-        this.selectedImage = null;
-      }
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imgSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    } else {
+      this.imgSrc = 'assets/Images/default.png';
+      this.selectedImage = null;
+    }
   }
   detectImages(event: any) {
-        this.selectedImagesList = event.target.files ;
-        console.log(this.selectedImagesList);
+    this.selectedImagesList = event.target.files;
+    console.log(this.selectedImagesList);
   }
 
   submit() {
-  //  const imagepath = '${this.selectedImage.name}_${new Date().getTime()}';
-   // console.log(imagepath);
+    //  const imagepath = '${this.selectedImage.name}_${new Date().getTime()}';
+    // console.log(imagepath);
   }
 }
